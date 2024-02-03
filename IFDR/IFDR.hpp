17965b108ad6f6de8f628445b556566fb65d751a
RamtinKosari <ramtinkosari@gmail.com>
2024-02-03 15:49:56 +0330

    [#REFACTOR] Add Individual Face Detection and Recognition Module

---
+++ IFDR/IFDR.hpp
@@ -0,0 +1,9 @@
+# ifndef KIDA_IFDR
+    /**
+     * @file IFDR.hpp
+     * @author Ramtin Kosari (ramtinkosari@gmail.com)
+     * @brief Kida Individual Face Detection and Recognition (IFDR) Header File
+     * @date 2024-01-24
+     */
+    # define KIDA_IFDR
+# endif // KIDA_IFDR
